7dac4dad9817a322a289887c0b64d31a6ec97704
Yurina Shin <yurinas@gmail.com>
2018-02-20 21:55:47 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,10 +9,11 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each do |project|
     projects[project] = {}
+    binding.pry
   end
   projects
 end
-binding.pry
+
 create_project_hash
 #projects: kickstarter.css("li.project.grid_4")
 #title: project.css("h2.bbcard_name strong a").text
